58438883a262ff7b60da7983f415cfbbc07f3c13
rkalau <rafal.kalata.itservice@gmail.com>
2021-01-13 00:18:25 +0100

    commit

--- src/main/java/pl/spring/controller/BasketController.java
+++ src/main/java/pl/spring/controller/BasketController.java
@@ -3,41 +3,37 @@ package pl.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.spring.model.Basket;
 import pl.spring.model.Product;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
 public class BasketController {
 
     private final Basket basket;
-    private List<Product> productList;
 
     @Autowired
     public BasketController(Basket basket) {
         this.basket = basket;
-        addProductToBasket(basket);
+        basket.addProductToBasket();
     }
 
-
-    public void addProductToBasket(Basket basket) {
-
-        productList = new ArrayList<>();
-        productList.add(new Product("Milk", basket.generateRandomPrice(50.00, 300.00)));
-        productList.add(new Product("Bread", basket.generateRandomPrice(50.00, 300.00)));
-        productList.add(new Product("Ham", basket.generateRandomPrice(50.00, 300.00)));
-        productList.add(new Product("Cheese", basket.generateRandomPrice(50.00, 300.00)));
-        productList.add(new Product("Apple", basket.generateRandomPrice(50.00, 300.00)));
-
-        basket.setProductList(productList);
+    @GetMapping(value ="/getProductList")
+    public List<Product> getProductList() {
+        return basket.getProductList();
+    }
+    @GetMapping(value ="/calculateValueBasket")
+    public Double calculateValueBasket() {
+        return basket.calculateValueBasket();
     }
 
     @EventListener(ApplicationReadyEvent.class)
         public void getBasketValue() {
 
-            for (Product product : productList) {
+            for (Product product : basket.getProductList()) {
                 System.out.println(product.getName() + " " + product.getPrice());
             }
             System.out.println("Total basket value: " + basket.calculateValueBasket());
--- src/main/java/pl/spring/model/Basket.java
+++ src/main/java/pl/spring/model/Basket.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -25,6 +26,7 @@ public class Basket {
     private double discountValue;
 
     public double calculateValueBasket() {
+
         double result = productList.stream()
                 .map(Product::getPrice)
                 .reduce(0.00, Double::sum);
@@ -39,5 +41,17 @@ public class Basket {
     public double generateRandomPrice(double min, double max) {
         double random = new Random().nextDouble();
         return Math.round((min + (random * (max - min)))*100.00)/100.00;
+    }
+
+    public void addProductToBasket() {
+
+        productList = new ArrayList<>();
+        productList.add(new Product("Milk", generateRandomPrice(50.00, 300.00)));
+        productList.add(new Product("Bread", generateRandomPrice(50.00, 300.00)));
+        productList.add(new Product("Ham", generateRandomPrice(50.00, 300.00)));
+        productList.add(new Product("Cheese", generateRandomPrice(50.00, 300.00)));
+        productList.add(new Product("Apple", generateRandomPrice(50.00, 300.00)));
+
+       setProductList(productList);
     }
 }
